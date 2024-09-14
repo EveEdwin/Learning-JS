7d10a6c7f0cfee43f05c9b76852e644bfac1966b
Mohit <me.mohitsharma14@gmail.com>
2024-09-14 17:43:34 +0530

    Learning JS

---
+++ Lecture 1/script.js
@@ -0,0 +1,7 @@
+// keyword vs word 
+
+// word --> iska matlab hota hai jiska kuch meaning nahi hai for the compiler 
+// eg ---> mohit, spark, computer
+
+// keyword --> iska kuch matlab hai and the compiler know what this means has some functions to perform 
+// eg --> console, if, alert,
---
+++ script.js
@@ -0,0 +1,29 @@
+function myFunction(p1, p2) {
+    return p1 * p2;
+  }
+console.log(myFunction);
+
+// Function expression
+
+
+// {
+//   country: "India",
+//   state:{
+//     stateName: "Bihar",
+//     districtName: {
+//       districtName: "Patna",
+//       age: "Total"
+
+      
+//     },
+//     districtName: {
+//       districtName: "Patna",
+//       population: 1234567890,
+//     },
+//     stateName: "",
+//     districtName: {
+//       districtName: "Patna",
+//       population: 1234567890,
+//     },
+//   },
+// }
