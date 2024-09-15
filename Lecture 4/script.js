96f657ea3a808cb6bc1148e9be7d61190683e370
Mohit <me.mohitsharma14@gmail.com>
2024-09-15 09:35:53 +0530

    Concepts added again

---
+++ Lecture 4/script.js
@@ -0,0 +1,21 @@
+// Stack and Heap memory 
+
+// Stack --> Primitive 
+
+
+// heap Memory  --- > reference datatype
+/******************************************************* */
+
+let anotherName = "another"
+anotherName = "newanother"
+
+console.log(anotherName);
+
+
+let user = {
+    email:"abc@gmail.com",
+    upi:"abc@upi",
+}
+
+console.log(user.email);
+console.log(user.upi);
